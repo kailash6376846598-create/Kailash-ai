be1b87e47a1e2cf29d4f166ca8175559e80b941b
kailash6376846598-create <kailash6376846598@gmail.com>
2026-08-02 08:12:45 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,37 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Kailash AI API is working" });
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ reply: "Method not allowed" });
+  }
+
+  const { prompt } = req.body;
+
+  const apiKey = process.env.GEMINI_API_KEY;
+
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: prompt,
+              },
+            ],
+          },
+        ],
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  const reply =
+    data.candidates?.[0]?.content?.parts?.[0]?.text || "No response";
+
+  res.status(200).json({ reply });
 }
