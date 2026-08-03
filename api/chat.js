2311a6b16f4d7433e523e9e17be201c23715f3b9
kailash6376846598-create <kailash6376846598@gmail.com>
2026-08-03 20:52:30 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ reply: "Method not allowed" });
   }
@@ -46,7 +46,3 @@ User: ${prompt}`,
     return res.status(200).json({ reply });
   } catch (error) {
     return res.status(500).json({
-      reply: error.message,
-    });
-  }
-}
