5d2f0a1f42bba8cf167d041cc0b6b20a093b289d
kailash6376846598-create <kailash6376846598@gmail.com>
2026-08-06 22:22:36 +0530

    Conditionally set PDF.js worker source

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let chatHistory = [];
 let pdfText = "";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.js";
+if (window.pdfjsLib) {
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.js";
+}
 function saveChat() {
   const responseDiv = document.getElementById("response");
   localStorage.setItem("kailash_chat", responseDiv.innerHTML);
